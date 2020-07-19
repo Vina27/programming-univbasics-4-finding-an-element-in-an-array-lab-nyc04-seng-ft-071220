d4bf5c7a7a31e08a8a5339ea92527a26cf4e1a36
Maysoon Tabali <mtab03@me.com>
2020-07-19 03:17:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -6,11 +6,3 @@ def find_element_index(array, value_to_find)
   end 
 end
 
-x = 1
-if x > 2
-   puts "x is greater than 2"
-elsif x <= 2 and x!=0
-   puts "x is 1"
-else
-   puts "I can't guess the number"
-end
