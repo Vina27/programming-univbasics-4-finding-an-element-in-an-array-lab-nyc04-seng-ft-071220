cdb1e46e6d4b425d84d9b89df7d9c159c7a8e1bd
Maysoon Tabali <mtab03@me.com>
2020-07-19 03:12:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,12 @@
 def find_element_index(array, value_to_find)
   
 end
+
+x = 1
+if x > 2
+   puts "x is greater than 2"
+elsif x <= 2 and x!=0
+   puts "x is 1"
+else
+   puts "I can't guess the number"
+end
